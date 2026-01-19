b6b17977bdff622c4a97426961625c63483a22d8
yahya mosa <yhea23112003@gamil.com>
2026-01-19 12:58:51 +0300

    add home page

--- lib/features/home/view/home.dart
+++ lib/features/home/view/home.dart
@@ -5,6 +5,9 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(title: const Text('Home')),
+      body: const Center(child: Text('Home')),
+    );
   }
 }
